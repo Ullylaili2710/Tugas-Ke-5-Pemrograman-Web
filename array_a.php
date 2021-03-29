30c619c98f7528217e2ac666281adf12a9fa4b6a
Ullylaili2710 <79493899+Ullylaili2710@users.noreply.github.com>
2021-03-29 21:12:14 +0700

    Add files via upload

---
+++ array_a.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Pemrograman PHP dengan Array</title>
+</head>
+<body>
+	<?php //perintah php
+	// Penulisan array dapat dibuat seperti berikut
+	$nama[] = "Ully"; //membuat variabel $nama dengan array yang menyimpan string Ully
+	$nama[] = "Laili"; //membuat variabel $nama dengan array yang menyimpan string Laili
+	$nama[] = "Musyarofah"; //membuat variabel $nama dengan array yang menyimpan string Musyarofah
+	echo $nama[1] . $nama[2] . $nama[0]; //menampilkan teks nilai variabel nama indeks ke 1, nilai variabel nama indeks ke 2, nilai variabel nama indeks ke 0 
+	echo "<br>"; //menampilkan ganti baris
+	?>
+
+</body>
+</html>
